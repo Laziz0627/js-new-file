f5f1d4d6cde51a311e0ab9e37311d662e0632717
Abdumajodov Laziz <153080975+Laziz0627@users.noreply.github.com>
2024-05-03 23:50:11 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,34 @@
+var studentAge = 19;
+var votingAge = 20;
+
+if (studentAge >= votingAge) {
+    console.log("You can vote");
+} else {
+    console.log("COme back later")
+}
+var studentScore = 87;
+if(studentScore >= 90) {
+    console.log("You got A")
+}
+
+
+
+let yosh = prompt("siz necha yoshdasiz?")
+let familya = prompt("sizning familangiz")
+let joy = prompt("sizning yashash joyingiz")
+
+if(yosh == 17,18) {
+    console.log('Ha')
+}else {
+    console.log('Kirish yoq')
+}
+if(familya == abdumajidov) {
+    console.log('Ha')
+}else{
+    console.log('kirish yoq')
+}
+if(joy == boston, jizzax, zarbdor) {
+    console.log('Ha')
+}else{
+    console.log('kirish yoq')
+}
